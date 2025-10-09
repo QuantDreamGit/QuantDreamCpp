5b40fff0bf6d0deb1d806a032a1be9aa91f2ebc3
QuantDreamGit <s283085@studenti.polito.it>
2025-10-09 11:29:51 +0200

    full robust measures' import

--- include/quantdream/statistics/robust/robustEstimators.h
+++ include/quantdream/statistics/robust/robustEstimators.h
@@ -5,4 +5,8 @@
 #ifndef QUANTDREAMCPP_ROBUSTESTIMATORS_H
 #define QUANTDREAMCPP_ROBUSTESTIMATORS_H
 
+#include "quantdream/statistics/robust/center/trimmed_mean.h"
+#include "quantdream/statistics/robust/center/winsorized_mean.h"
+
+
 #endif  // QUANTDREAMCPP_ROBUSTESTIMATORS_H
