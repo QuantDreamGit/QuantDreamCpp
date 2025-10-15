8954217d999d44c859f9cc81184a789a495c0b45
QuantDream <priolo.giuseppe01@proton.me>
2025-10-15 10:07:16 +0200

    fixed main test

---
+++ test/source/main_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by user on 10/15/25.
+//
+#include "ibwrapper_test/test.h"
+
+int main() {
+  IBWrapper ib;
+  ib.connect("address", 0000, 123);
+  return 0;
+}
--- test/source/test.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by user on 10/15/25.
-//
-#include "test"
