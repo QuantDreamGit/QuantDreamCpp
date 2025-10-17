5f4b7eb871e486c1ec4ceea9a0fab5468dee0267
QuantDreamGit <s283085@studenti.polito.it>
2025-10-17 20:07:52 +0200

    added getContractDetails and getOptionChain

--- standalone/source/ibkr/get_market_data.cpp
+++ standalone/source/ibkr/get_market_data.cpp
@@ -1,22 +1,16 @@
-#include "wrappers/IBWrapperBase.h"
+#include "../../../external/IBWrapper/include/request/contracts/ContractDetails.h"
+#include "../../../external/IBWrapper/include/request/market_data/MarketDataRequests.h"
 #include "contracts/StockContracts.h"
-#include "request/MarketDataRequests.h"
+#include "request/options/OptionChain.h"
+#include "wrappers/IBWrapperBase.h"
 
 int main() {
   IBWrapperBase ib;
 
   if (!ib.connect()) { return 1; }
 
-  // Create a stock contract for GOOG
-  // Contract stock = IB::Contracts::makeStock("GOOGL");
-  // IB::Requests::requestMarketData(ib.client, 3, stock);
+  IB::Options::ChainInfo optChain = IB::Request::getOptionChain(ib, IB::Contracts::makeStock("AAPL"));
 
-  // Create an option contract for GOOGL 20251215 150 Call
-  // Contract option = IB::Contracts::makeOption("GOOGL", "20251015", 250.0, "C");
-  // IB::Requests::requestMarketData(ib.client, 3, option, 2001);
-
-  ib.client -> reqContractDetails(999, IB::Contracts::makeStock("AAPL"));
-  ib.client -> reqSecDefOptParams(9001, "AAPL", "", "STK", 265598);
   std::cout << "Press Enter to exit..." << std::endl;
   std::cin.get();
 
