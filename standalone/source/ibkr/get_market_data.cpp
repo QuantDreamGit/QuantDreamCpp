a8ed8c72de7e15a4f823f8be3145f80dca2eed0a
QuantDreamGit <s283085@studenti.polito.it>
2025-10-25 05:53:03 +0200

    added market data retrieval and order execution example

--- standalone/source/ibkr/get_market_data.cpp
+++ standalone/source/ibkr/get_market_data.cpp
@@ -1,15 +1,56 @@
 #include "../../../external/IBWrapper/include/request/contracts/ContractDetails.h"
 #include "../../../external/IBWrapper/include/request/market_data/MarketDataRequests.h"
+#include "Order.h"
 #include "contracts/StockContracts.h"
-#include "request/options/OptionChain.h"
+#include "helpers/connection.h"
+#include "orders/common_orders.h"
+#include "orders/management/open.h"
+#include "orders/options/simple_order.h"
+#include "request/market_data/get_prices.h"
+#include "request/options/chain.h"
+#include "request/options/greeks.h"
 #include "wrappers/IBWrapperBase.h"
 
 int main() {
+  // Logger setup
+  Logger::setEnabled(true);
+  Logger::setLevel(Logger::Level::INFO);
+
+  // IB Wrapper setup
   IBWrapperBase ib;
+  std::string exchange = "CBOE";
+  // Connect to IB Gateway or TWS
+  IB::Helpers::ensureConnected(ib, "127.0.0.1", 4002, 0);
+  // Set market data type to real-time
+  ib.client->reqMarketDataType(1);
 
-  if (!ib.connect()) { return 1; }
+  // Get the underlying contract
+  Contract const underlying = IB::Contracts::makeStock("GOOGL", exchange, "USD");
 
-  IB::Options::ChainInfo optChain = IB::Request::getOptionChain(ib, IB::Contracts::makeStock("AAPL"));
+  // Get chain info
+  const IB::Options::ChainInfo optChain = IB::Request::getOptionChain(ib,
+                                                                   underlying,
+                                                                   IB::ReqId::OPTION_CHAIN_ID,
+                                                                   0.1,
+                                                                   exchange);
+
+  // Create a market order
+  const Order marketOrder = IB::Orders::MarketBuy(1);
+
+  // Execute simple option order
+  IB::Orders::Options::placeSimpleOrder(ib, underlying, optChain, marketOrder, "C");
+
+  // Check if an order is currently open
+  // Since it's not possible to be sure when open orders are finished a get function is required to
+  // fetch them from Wrapper buffer.
+  // Furthermore, when requesting order the buffer is always updated
+  IB::Orders::Management::Open::requestClientOpenOrders(ib);
+  auto openOrders = ib.getOpenOrders();
+
+  // Cancel all orders
+  // IB::Orders::Management::Open::cancelAll(ib);
+
+  // auto callGreeks = IB::Requests::getGreeksTable(ib, underlying, optChain, "C");
 
   std::cout << "Press Enter to exit..." << std::endl;
   std::cin.get();
