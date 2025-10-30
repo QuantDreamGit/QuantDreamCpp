85161d57ebcd110f2c815c67d03102014d27bf46
QuantDreamGit <s283085@studenti.polito.it>
2025-10-30 17:54:05 +0100

    add IBBaseWrapper and related classes for improved wrapper functionality and position management

--- standalone/source/ibkr/get_market_data.cpp
+++ standalone/source/ibkr/get_market_data.cpp
@@ -2,35 +2,28 @@
 #include "helpers/connection.h"
 #include "helpers/open_markets.h"
 #include "orders/management/open.h"
+#include "orders/management/pnl.h"
+#include "orders/management/position.h"
 #include "orders/options/condor_order.h"
 #include "request/options/chain.h"
-#include "wrappers/IBWrapperBase.h"
+#include "wrappers/IBBaseWrapper.h"
+#include "wrappers/IBStrategyWrapper.h"
 
 int main() {
   // Logger setup
   Logger::setEnabled(true);
-  Logger::setLevel(Logger::Level::DEBUG);
+  Logger::setLevel(Logger::Level::TIMER);
 
   // Constants
   const std::string exchange = "SMART";
 
   // IB Wrapper setup
-  IBWrapperBase ib;
+  IBStrategyWrapper ib;
   // Connect to IB Gateway or TWS (by default it uses realtime market data)
   IB::Helpers::ensureConnected(ib, "127.0.0.1", 4002, 0);
 
   // Get the underlying contract
   Contract const underlying = IB::Contracts::makeStock("GOOGL", exchange, "USD");
-
-  if (!IB::Helpers::isMarketOpen(ib, underlying)) {
-    LOG_WARN("[MAIN] Market is CLOSED — skipping data and order requests.");
-    std::cout << "Market is closed. Press Enter to exit..." << std::endl;
-    std::cin.get();
-    ib.disconnect();
-    return 0;
-  }
-
-  LOG_INFO("[MAIN] Market is OPEN — proceeding with requests.");
 
   // Get chain info
   const IB::Options::ChainInfo optChain = IB::Request::getOptionChain(ib,
@@ -45,8 +38,9 @@ int main() {
   // IB::Orders::Options::placeSimpleOrder(ib, underlying, optChain, marketOrder, "C");
 
   // Cancel all orders
-  IB::Orders::Management::Open::cancelAll(ib);
+  // IB::Orders::Management::Open::cancelAll(ib);
 
+  /*
   IB::Orders::Options::placeIronCondor(
       ib,
       underlying,
@@ -58,10 +52,36 @@ int main() {
       0.1,
       true                              // Confirms auto-strikes usage
   );
+  */
 
+  // ib.client->reqAccountSummary(
+  //   9001,           // reqId (any unique integer)
+  //   "All",          // group ("All" = all accounts)
+  //   "NetLiquidation,TotalCashValue,BuyingPower,AvailableFunds"
+  // );
+
+  // auto positions = IB::Orders::Management::getOpenPositions(ib);
+  // IB::Orders::Management::closeAllPositions(ib);
+  // IB::Orders::Management::showCurrentPnL(ib);
+
+  IB::Orders::Management::closeAllPositions(ib);
+  /*
+  while (true) {
+    try {
+      IB::Orders::Management::showCurrentPnL(ib);
+    } catch (const std::exception& e) {
+      LOG_ERROR("[PnLLoop] Exception: ", e.what());
+    }
+
+    // Sleep for 1 second between refreshes
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  }
+  IB::Orders::Management::closeAllPositions(ib);
+  */
+  // ib.disconnect();
   // Cancel all orders after waiting for one second
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  IB::Orders::Management::Open::cancelAll(ib);
+  // std::this_thread::sleep_for(std::chrono::seconds(1));
+  // IB::Orders::Management::Open::cancelAll(ib);
 
 
 
